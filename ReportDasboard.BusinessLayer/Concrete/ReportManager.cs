dc6766ebc12b9b980b5cd04d350c64e00b1a4357
İsmail Baran <ismailbaran04@gmail.com>
2024-05-10 17:37:45 +0300

    hatalıda olsa çalıştığı içiin projeye eklendi

---
+++ ReportDasboard.BusinessLayer/Concrete/ReportManager.cs
@@ -0,0 +1,46 @@
+﻿using ReportDashboard.BusinessLayer.Abstarct;
+using ReportDashboard.DataAccessLayer.Abstract;
+using ReportDashboard.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportDashboard.BusinessLayer.Concrete
+{
+    public class ReportManager : IReportService
+    {
+        private readonly IReportDal _reportDal;
+
+        public ReportManager(IReportDal reportDal)
+        {
+            _reportDal = reportDal;
+        }
+
+        public void TAdd(Report entity)
+        {
+            _reportDal.Add(entity);
+        }
+
+        public void TDelete(Report entity)
+        {
+            _reportDal.Delete(entity);
+        }
+
+        public Report TGetByID(int id)
+        {
+            return _reportDal.GetByID(id);
+        }
+
+        public List<Report> TGetListAll()
+        {
+            return _reportDal.GetListAll();
+        }
+
+        public void TUpdate(Report entity)
+        {
+            _reportDal.Update(entity);
+        }
+    }
+}
