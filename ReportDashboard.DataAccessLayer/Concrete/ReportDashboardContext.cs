2b0d919c4ea31347cd98dd9e3647a12b39a29d33
İsmail Baran <ismailbaran04@gmail.com>
2024-05-10 15:47:41 +0300

    Data access layer eklendş

---
+++ ReportDashboard.DataAccessLayer/Concrete/ReportDashboardContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReportDashboard.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportDashboard.DataAccessLayer.Concrete
+{
+    public class ReportDashboardContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-AV376HC\\SQLEXPRESS; Initial Catalog=ReportDb; Integrated Security=True; TrustServerCertificate=True") ;
+        }
+        public DbSet<DbTable> DbTables{ get; set; }
+        public DbSet<Report> Reports{ get; set; }
+    }
+}
---
+++ ReportDashboard.DataAccessLayer/Migrations/20240510121717_initialize.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReportDashboard.DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialize : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DbTables",
+                columns: table => new
+                {
+                    DbTableID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DbTable_ServerName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DbTable_UserName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DbTable_Password = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DbTables", x => x.DbTableID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reports",
+                columns: table => new
+                {
+                    ReportID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ReportName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reports", x => x.ReportID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DbTables");
+
+            migrationBuilder.DropTable(
+                name: "Reports");
+        }
+    }
+}
