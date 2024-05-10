4ed53a62e2d469274beff325059b94239abfe34e
İsmail Baran <ismailbaran04@gmail.com>
2024-05-10 15:48:29 +0300

    entity katmanı eklendi

---
+++ ReportDashboard.EntityLayer/Entities/Report.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportDashboard.EntityLayer.Entities
+{
+    public class Report
+    {
+        [Key]
+        public int ReportID { get; set; }
+        public string ReportName { get; set; }
+    }
+}
