431a9d5c4ca785b86557cfe8fadbb7917c317866
İsmail Baran <ismailbaran04@gmail.com>
2024-05-10 17:21:37 +0300

    dtolar eklendş

---
+++ ReportDashboard.DtoLayer/DbTableDto/CreateDbTableDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportDashboard.DtoLayer.DbTableDto
+{
+    public class CreateDbTableDto
+    {
+        public string DbTable_ServerName { get; set; }
+        public string? DbTable_UserName { get; set; }
+        public string? DbTable_Password { get; set; }
+    }
+}
---
+++ ReportDashboard.DtoLayer/ReportDto/CreateReportDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportDashboard.DtoLayer.ReportDto
+{
+    public class CreateReportDto
+    {
+        public string ReportName { get; set; }
+    }
+}
