dd9632eb1d14f7fca212040c45b8ebf744394382
İsmail Baran <ismailbaran04@gmail.com>
2024-05-13 22:38:22 +0300

    veri tabanı ile ilgili tablolar işlendi yeni migrationlar yapıldı

--- ReportDashboard.DtoLayer/DbTableDto/UpdateDbTableDto.cs
+++ ReportDashboard.DtoLayer/DbTableDto/UpdateDbTableDto.cs
@@ -12,6 +12,7 @@ namespace ReportDashboard.DtoLayer.DbTableDto
         [Key]
         public int DbTableID { get; set; }
         public string DbTable_ServerName { get; set; }
+        public string DbTable_Database { get; set; }
         public string? DbTable_UserName { get; set; }
         public string? DbTable_Password { get; set; }
     }
