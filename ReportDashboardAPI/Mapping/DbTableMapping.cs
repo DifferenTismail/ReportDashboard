6637916bd635def93bc98999563182c144f1468c
İsmail Baran <ismailbaran04@gmail.com>
2024-05-13 09:04:19 +0300

    controllerı eklenen elemanlar maplendi

---
+++ ReportDashboardAPI/Mapping/DbTableMapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using ReportDashboard.DtoLayer.DbTableDto;
+using ReportDashboard.EntityLayer.Entities;
+
+namespace ReportDashboardAPI.Mapping
+{
+    public class DbTableMapping :Profile
+    {
+        public DbTableMapping()
+        {
+            CreateMap<DbTable, ResultDbTableDto>().ReverseMap();
+            CreateMap<DbTable, CreateDbTableDto>().ReverseMap();
+            CreateMap<DbTable, GetDbTableDto>().ReverseMap();
+            CreateMap<DbTable, UpdateDbTableDto>().ReverseMap();
+        }
+    }
+}
---
+++ ReportDashboardAPI/Mapping/ReportMapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using ReportDashboard.EntityLayer.Entities;
+using ReportDashboard.DtoLayer.ReportDto;
+
+namespace ReportDashboardAPI.Mapping
+{
+    public class ReportMapping: Profile
+    {
+        public ReportMapping()
+        {
+            CreateMap<Report, ResultReportDto>().ReverseMap();
+            CreateMap<Report, CreateReportDto>().ReverseMap();
+            CreateMap<Report, GetReportDto>().ReverseMap();
+            CreateMap<Report, UpdateReportDto>().ReverseMap();
+        }
+    }
+}
