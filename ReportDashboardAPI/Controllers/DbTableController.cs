38adcb2c3022adaada9b18dca4d5175ec06a20d2
İsmail Baran <ismailbaran04@gmail.com>
2024-05-13 09:03:47 +0300

    controllerlar eklendi

---
+++ ReportDashboardAPI/Controllers/DbTableController.cs
@@ -0,0 +1,65 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ReportDashboard.BusinessLayer.Abstarct;
+using ReportDashboard.DtoLayer.DbTableDto;
+using ReportDashboard.EntityLayer.Entities;
+
+namespace ReportDashboardAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DbTableController : ControllerBase
+    {
+        private readonly IDbTableService _dbTableService;
+
+        public DbTableController(IDbTableService dbTableService)
+        {
+            _dbTableService = dbTableService;
+        }
+        [HttpGet("DbTableList")]
+        public IActionResult DbTableList()
+        {
+            var values = _dbTableService.TGetListAll();
+            return Ok(values);
+        }
+        [HttpPost("CreateDbTable")]
+        public IActionResult CreateDbTable(CreateDbTableDto createDbTableDto)
+        {
+            DbTable dbTable = new DbTable()
+            {
+                DbTable_ServerName = createDbTableDto.DbTable_ServerName,
+                DbTable_UserName = createDbTableDto.DbTable_UserName,
+                DbTable_Password = createDbTableDto.DbTable_Password
+            };
+            _dbTableService.TAdd(dbTable);
+            return Ok("Veri Tabanı Başarılı Bir Şekilde Kayıt Edildi");
+        }
+        [HttpDelete("DbTableDelete")]
+        public IActionResult DeleteDbTable(int id)
+        {
+            var value = _dbTableService.TGetByID(id);
+            _dbTableService.TDelete(value);
+            return Ok("Veri Tabanı Başarılı Bir Şekilde Silindi");
+        }
+        [HttpPut("DbTableUpdate")]
+        public IActionResult DbTableUpdate(UpdateDbTableDto updateDbTableDto)
+        {
+            DbTable dbTable = new DbTable()
+            {
+                DbTableID = updateDbTableDto.DbTableID,
+                DbTable_ServerName = updateDbTableDto.DbTable_ServerName,
+                DbTable_UserName = updateDbTableDto.DbTable_UserName,
+                DbTable_Password = updateDbTableDto.DbTable_Password
+            };
+            _dbTableService.TUpdate(dbTable);
+            return Ok("Veri Tabanı Bilgileri Başarılı Bir Şekilde Güncellendi");
+        }
+        [HttpGet("GetDbTable")]
+        public IActionResult GetDbTable(int id)
+        {
+            var value = _dbTableService.TGetByID(id);
+            return Ok(value);
+        }
+    }
+}
---
+++ ReportDashboardAPI/Controllers/ReportController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ReportDashboard.BusinessLayer.Abstarct;
+using ReportDashboard.DtoLayer.ReportDto;
+using ReportDashboard.EntityLayer.Entities;
+
+namespace ReportDashboardAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportController : ControllerBase
+    {
+        private readonly IReportService _reportService;
+
+        public ReportController(IReportService reportService)
+        {
+            _reportService = reportService;
+        }
+        [HttpGet("ReportList")]
+        public IActionResult ReportList()
+        {
+            var values = _reportService.TGetListAll();
+            return Ok(values);
+        }
+        [HttpPost("CreateReport")]
+        public IActionResult CreateReport(CreateReportDto createReportDto) 
+        {
+            Report report = new Report()
+            {
+                ReportName = createReportDto.ReportName
+            };
+            _reportService.TAdd(report);
+            return Ok("Rapor Başarılı Bir Şekilde Kayıt Edildi");
+        }
+        [HttpDelete("ReportDelete")]
+        public IActionResult DeleteReport(int id)
+        {
+            var value = _reportService.TGetByID(id);
+            _reportService.TDelete(value);
+            return Ok("Rapor Alanı Başarılı Bir Şekilde Silindi");
+        }
+
+        [HttpPut("UpdateReport")]
+        public IActionResult UpdateReport(UpdateReportDto updateReportDto)
+        {
+            Report report = new Report()
+            {
+                ReportID = updateReportDto.ReportID,
+                ReportName = updateReportDto.ReportName
+            };
+            _reportService.TUpdate(report);
+            return Ok("Rapor Alanı Başarılı Bir Şekilde Güncellendi");
+        }
+        [HttpGet("GetReport")]
+        public IActionResult GetReport(int id) 
+        {
+            var value = _reportService.TGetByID(id);
+            return Ok(value);
+        }
+    }
+}
