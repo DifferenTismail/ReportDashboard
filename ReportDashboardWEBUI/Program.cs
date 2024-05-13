40b36ec774eb948b8fdf2cb03e6caac47f1264aa
İsmail Baran <ismailbaran04@gmail.com>
2024-05-13 22:42:38 +0300

    program cs de dosya yolu düzenlendi

--- ReportDashboardWEBUI/Program.cs
+++ ReportDashboardWEBUI/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Connection}/{action=Index}/{id?}");
 
 app.Run();
