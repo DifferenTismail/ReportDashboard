bbd80bc08fffc33b286b8524d2187451ed15e34b
İsmail Baran <ismailbaran04@gmail.com>
2024-05-14 12:04:02 +0300

    yeni entitiynin imzaları atıldı ve otomatik güncellemeler işlendi

---
+++ ReportDasboard.BusinessLayer/Concrete/WriteQueryManager.cs
@@ -0,0 +1,46 @@
+﻿using ReportDashboard.BusinessLayer.Abstarct;
+using ReportDashboard.DataAccessLayer.Abstract;
+using ReportDashboard.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportDashboard.BusinessLayer.Concrete
+{
+    public class WriteQueryManager : IWriteQueryService
+    {
+        private readonly IWriteQueryDal _writeQueryDal;
+
+        public WriteQueryManager(IWriteQueryDal writeQueryDal)
+        {
+            _writeQueryDal = writeQueryDal;
+        }
+
+        public void TAdd(WriteQuery entity)
+        {
+            _writeQueryDal.Add(entity);
+        }
+
+        public void TDelete(WriteQuery entity)
+        {
+            _writeQueryDal.Delete(entity);
+        }
+
+        public WriteQuery TGetByID(int id)
+        {
+            return _writeQueryDal.GetByID(id);
+        }
+
+        public List<WriteQuery> TGetListAll()
+        {
+            return _writeQueryDal.GetListAll();
+        }
+
+        public void TUpdate(WriteQuery entity)
+        {
+            _writeQueryDal.Update(entity);
+        }
+    }
+}
