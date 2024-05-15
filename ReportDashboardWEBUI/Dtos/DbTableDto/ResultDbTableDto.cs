d0bf308fa3ca2023211470ddf676246d0a1cdc45
İsmail Baran <ismailbaran04@gmail.com>
2024-05-15 23:06:50 +0300

    apideki dto uı a taşındı ve yeni view açıldı

---
+++ ReportDashboardWEBUI/Dtos/DbTableDto/ResultDbTableDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReportDashboardWEBUI.Dtos.DbTableDto
+{
+    public class ResultDbTableDto
+    {
+        [Key]
+        public int DbTableID { get; set; }
+        public string DbTable_ServerName { get; set; }
+        public string DBMS_Name { get; set; }
+        public string DbTable_Database { get; set; }
+        public string? DbTable_UserName { get; set; }
+        public string? DbTable_Password { get; set; }
+    }
+}
