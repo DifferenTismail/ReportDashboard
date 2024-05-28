e2368247dc9074238a477b84d087af620ba6d3d3
İsmail Baran <ismailbaran04@gmail.com>
2024-05-28 13:52:47 +0300

    otomaik güncellemeler işlendi

--- ReportDashboardAPI/obj/Debug/net8.0/ReportDashboardAPI.AssemblyInfo.cs
+++ ReportDashboardAPI/obj/Debug/net8.0/ReportDashboardAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReportDashboardAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54ef412d5c2d75c7bd7998c124ca45b7223a71ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+707394e429ff10d514f14c06f7f0f6a05210f074")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReportDashboardAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReportDashboardAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
