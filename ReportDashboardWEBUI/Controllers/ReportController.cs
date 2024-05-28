1de4ebbba92042e092171f5ceec934b81a9b9eda
İsmail Baran <ismailbaran04@gmail.com>
2024-05-28 13:54:32 +0300

    rapor oluşturma sayfaası iççin controller açıldı

---
+++ ReportDashboardWEBUI/Controllers/ReportController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ReportDashboardWEBUI.Controllers
+{
+    public class ReportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CreateReport()
+        {
+            return View();
+        }
+    }
+}
